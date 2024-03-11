55dab18fc6994c8c8bd93e2d67664aab51fb1807
Ross Hadden <rosshadden@gmail.com>
2024-03-11 13:07:32 -0400

    feat: misc improvements

--- keymap.c
+++ keymap.c
@@ -14,8 +14,6 @@ enum sofle_layers {
   _LOWER,
   _RAISE,
   _LEGACY,
-
-  _BLANK,
 };
 
 enum custom_keycodes {
@@ -117,7 +115,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT(
     KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_BACKSLASH,
     KC_EQUAL,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_MINUS,
-    _______,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                       KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
+    _______,   LGUI_T(KC_A),   LALT_T(KC_S),    LCTL_T(KC_D),    LSFT_T(KC_F),    KC_G,                       KC_H,    RSFT_T(KC_J),    RCTL_T(KC_K),    RALT_T(KC_L), RGUI_T(KC_SCLN),  KC_QUOT,
     _______,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    _______, KC_ERMAK, KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH,  KC_RSFT,
                       _______, _______, _______, _______, _______, _______,  _______, _______, _______, _______
   ),
@@ -131,18 +129,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ├────┼───┼───┼───┼───┼───┤               ├───┼───┼───┼───┼───┼────┤
     │    │ # │ $ │ ( │ ) │ ` │               │QMK│ 4 │ 5 │ 6 │ + │    │
     ├────┼───┼───┼───┼───┼───┼───┐       ┌───┼───┼───┼───┼───┼───┼────┤
-    │    │ # │ $ │ [ │ ] │ ~ │   │       │   │QMK│ 1 │ 2 │ 3 │ / │    │
+    │    │ < │ > │ [ │ ] │ ~ │   │       │   │QMK│ 1 │ 2 │ 3 │ / │    │
     └────┴───┴───┴───┴───┴───┴───┘       └───┴───┴───┴───┴───┴───┴────┘
              ┌───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┐
-             │   │   │   │   │   │       │ 0 │ 0 │ 0 │ 0 │ 0 │
+             │   │   │   │   │   │       │   │   │ 0 │ 0 │ 0 │
              └───┴───┴───┴───┴───┘       └───┴───┴───┴───┴───┘
   */
   [_LOWER] = LAYOUT(
-    QK_BOOT,         KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                      KC_F6,   KC_F7, KC_F8, KC_F9, KC_F10,  KC_F11,
-    QK_CLEAR_EEPROM, KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_PIPE,                    DT_PRNT, KC_7,  KC_8,  KC_9,  KC_ASTR, KC_F12,
-    _______,         KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_GRV,                     DT_UP,   KC_4,  KC_5,  KC_6,  KC_PPLS, XXXXXXX,
-    _______,         KC_HASH, KC_DLR,  KC_LBRC, KC_RBRC, KC_TILD, _______,  _______, DT_DOWN, KC_1,  KC_2,  KC_3,  KC_PSLS, _______,
-                              _______, _______, _______, _______, _______,  _______, KC_P0,   KC_P0, KC_P0, KC_P0
+    QK_BOOT,         KC_F1,                 KC_F2,                 KC_F3,   KC_F4,   KC_F5,                      KC_F6,   KC_F7, KC_F8, KC_F9, KC_F10,  KC_F11,
+    QK_CLEAR_EEPROM, KC_EXLM,               KC_AT,                 KC_LCBR, KC_RCBR, KC_PIPE,                    DT_PRNT, KC_7,  KC_8,  KC_9,  KC_ASTR, KC_F12,
+    _______,         KC_HASH,               KC_DLR,                KC_LPRN, KC_RPRN, KC_GRV,                     DT_UP,   KC_4,  KC_5,  KC_6,  KC_PPLS, XXXXXXX,
+    _______,         KC_LEFT_ANGLE_BRACKET, KC_LEFT_ANGLE_BRACKET, KC_LBRC, KC_RBRC, KC_TILD, _______,  _______, DT_DOWN, KC_1,  KC_2,  KC_3,  KC_PSLS, _______,
+                                            _______,               _______, _______, _______, _______,  _______, _______, KC_P0, KC_P0, KC_P0
   ),
 
   /*
@@ -161,11 +159,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                    └───┴───┴───┴───┴───┘     └───┴───┴───┴───┴───┘
   */
   [_RAISE] = LAYOUT(
-    XXXXXXX, KC_ACL0, KC_ACL1, KC_ACL2, XXXXXXX, QK_MAKE,                     XXXXXXX, XXXXXXX, RGB_MOD, RGB_RMOD, RGB_HUI, XXXXXXX,
-    XXXXXXX, KC_BTN2, XXXXXXX, KC_MS_U, XXXXXXX, XXXXXXX,                     XXXXXXX, KC_BTN1, KC_UP,   KC_BTN2,  XXXXXXX, XXXXXXX,
-    CW_TOGG, KC_BTN1, KC_MS_L, KC_MS_D, KC_MS_R, XXXXXXX,                     XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT,  XXXXXXX, XXXXXXX,
-    _______, KC_BTN3, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, _______,
-                      _______, _______, _______, KC_DELETE, _______, _______, _______, _______, _______, _______
+    XXXXXXX, KC_ACL0, KC_ACL1, KC_ACL2, XXXXXXX, QK_MAKE,                   XXXXXXX, XXXXXXX, RGB_MOD, RGB_RMOD, RGB_HUI, XXXXXXX,
+    XXXXXXX, KC_BTN2, XXXXXXX, KC_MS_U, XXXXXXX, XXXXXXX,                   XXXXXXX, KC_BTN1, KC_UP,   KC_BTN2,  XXXXXXX, XXXXXXX,
+    CW_TOGG, KC_BTN1, KC_MS_L, KC_MS_D, KC_MS_R, XXXXXXX,                   XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT,  XXXXXXX, XXXXXXX,
+    _______, KC_BTN3, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, _______,
+                      KC_BTN2, KC_BTN3, KC_BTN1, _______, _______, _______, _______, _______, _______, _______
   ),
 
   /*
@@ -206,12 +204,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                    │   │   │   │   │   │     │   │   │   │   │   │
                    └───┴───┴───┴───┴───┘     └───┴───┴───┴───┴───┘
   */
-  [_BLANK] = LAYOUT(
-    _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-                      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+  // [_BLANK] = LAYOUT(
+  //   _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
+  //   _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
+  //   _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
+  //   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+  //                     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+  //
+  // ),
+  // [_BLANK] = LAYOUT(
+  //   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  //   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  //   _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
+  //   _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
+  //                     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
   ),
 };
 
@@ -332,7 +338,5 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
   [_LOWER] = { ENCODER_CCW_CW(_______, _______), ENCODER_CCW_CW(_______, _______) },
   [_RAISE] = { ENCODER_CCW_CW(_______, _______), ENCODER_CCW_CW(_______, _______) },
   [_LEGACY] = { ENCODER_CCW_CW(_______, _______), ENCODER_CCW_CW(_______, _______) },
-
-  [_BLANK] = { ENCODER_CCW_CW(_______, _______), ENCODER_CCW_CW(_______, _______) },
 };
 #endif
